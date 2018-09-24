ced87e3d1ef019d28f5b97aa0a822c43aa8c75fb
David Podbregar <david.podbregar@cartargetgroup.com>
2018-09-24 14:59:09 +0200

    - testing breakpoints

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -4,6 +4,7 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
+    debugger;
     const msg = 'new message';
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
